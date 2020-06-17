12c4fb1c65acbe5ac446e0ed106d807f3bd09d9b
ThiryS <thiry.simon1@gmail.com>
2020-06-17 11:44:40 +0200

    left boots, bow and elves

--- js/scripts.js
+++ js/scripts.js
@@ -63,16 +63,17 @@
       perso1.resistance = perso1.resistance * 0.8;
     }
     if (perso1.item.item === "Sword") {
-      perso1.maxDamage = perso1.maxDamage * 1.3;
+      perso1.boostDamage = perso1.boostDamage * 1.3;
     }
     if (perso1.item.item === "Staff") {
-      perso1.maxHealing = perso1.maxHealing * 1.2;
+      perso1.boostHeal = perso1.boostHeal * 1.2;
     }
     document.getElementById("validBtn1").classList.add("btn-success");
     document.getElementById("validBtn1").classList.remove("btn-primary");
     document.getElementById("form1").classList.add("hide");
     document.getElementById("table1").classList.remove("hide");
     document.getElementById("btn1").classList.remove("hide");
+    document.getElementById("healthbar1").classList.remove("hide");
     document.getElementById("tdname1").innerHTML = perso1.name;
     document.getElementById("tditem1").innerHTML = perso1.item.item;
     document.getElementById("tdrace1").innerHTML = perso1.race.item;
@@ -157,16 +158,17 @@
         perso2.resistance = perso2.resistance * 0.8;
     }
     if (perso2.item.item === "Sword") {
-      perso2.maxDamage = perso2.boostDamage * 1.3;
+      perso2.boostDamage = perso2.boostDamage * 1.3;
     }
     if (perso2.item.item === "Staff") {
-    perso2.maxHealing = perso2.boostHeal * 1.2;
+    perso2.boostHeal = perso2.boostHeal * 1.2;
     }
     document.getElementById("validBtn2").classList.add("btn-success");
     document.getElementById("validBtn2").classList.remove("btn-primary");
     document.getElementById("form2").classList.add("hide");
     document.getElementById("table2").classList.remove("hide");
     document.getElementById("btn2").classList.remove("hide");
+    document.getElementById("healthbar2").classList.remove("hide");
     document.getElementById("tdname2").innerHTML = perso2.name;
     document.getElementById("tditem2").innerHTML = perso2.item.item;
     document.getElementById("tdrace2").innerHTML = perso2.race.item;
@@ -198,6 +200,17 @@
       document.getElementById("heal2").disabled = false;
       document.getElementById("yield").disabled = true;
       document.getElementById("yield2").disabled = false;
+      if (perso2.race.item === 'Vampires') {
+        lifesteal(perso2,perso1);
+        let health1 = document.getElementById("health");
+        health1.value = perso1.currenthealth;
+        let health2 = document.getElementById("health2");
+        health2.value = perso2.currenthealth;
+        document.getElementById("health2").textContent = perso2.currenthealth;
+        document.getElementById("life2").textContent = `${document.getElementById("health2").value}/${document.getElementById("health2").max}`;
+        document.getElementById("health").textContent = perso1.currenthealth;
+        document.getElementById("life1").textContent = `${document.getElementById("health").value}/${document.getElementById("health").max}`;
+      }
     });
   });
   var allbtn2 = document.querySelectorAll(".actbtn2");
@@ -209,6 +222,17 @@
       document.getElementById("heal2").disabled = true;
       document.getElementById("yield").disabled = false;
       document.getElementById("yield2").disabled = true;
+      if (perso1.race.item === 'Vampires') {
+        lifesteal(perso1,perso2);
+        let health1 = document.getElementById("health");
+        health1.value = perso1.currenthealth;
+        let health2 = document.getElementById("health2");
+        health2.value = perso2.currenthealth;
+        document.getElementById("health2").textContent = health2.value;
+        document.getElementById("life2").textContent = `${document.getElementById("health2").value}/${document.getElementById("health2").max}`;
+        document.getElementById("health").textContent = health1.value;
+        document.getElementById("life1").textContent = `${document.getElementById("health").value}/${document.getElementById("health").max}`;
+      }
     });
   });
 
@@ -217,22 +241,32 @@
     health.value = hit(perso2,perso1);
     document.getElementById("health").textContent = health.value;
     document.getElementById("life1").textContent = `${document.getElementById("health").value}/${document.getElementById("health").max}`;
+    if (perso1.currenthealth === 0) {
+      alert(`${perso1.name} has no more life ${perso2.name} won! The game will restart`)
+      window.location.reload(false); 
+    }
   });
   document.getElementById("hit").addEventListener("click", function () {
     let health2 = document.getElementById("health2");
-    health2.value = hit(perso1,perso2); //Or whatever you want to do with it.
+    health2.value = hit(perso1,perso2);
     document.getElementById("health2").textContent = health.value;
     document.getElementById("life2").textContent = `${document.getElementById("health2").value}/${document.getElementById("health2").max}`;
+    if (perso2.currenthealth === 0) {
+      alert(`${perso2.name} has no more life ${perso1.name} won! The game will restart`)
+      window.location.reload(false); 
+    }
   });
   document.getElementById("heal").addEventListener("click", function () {
     let health = document.getElementById("health");
-    health.value += 10; //Or whatever you want to do with it.
+    health.value = heal(perso1);
     document.getElementById("health").textContent = health.value;
+    document.getElementById("life1").textContent = `${document.getElementById("health").value}/${document.getElementById("health").max}`;
   });
   document.getElementById("heal2").addEventListener("click", function () {
     let health2 = document.getElementById("health2");
-    health2.value += 10; //Or whatever you want to do with it.
+    health2.value = heal(perso2);
     document.getElementById("health2").textContent = health.value;
+    document.getElementById("life2").textContent = `${document.getElementById("health2").value}/${document.getElementById("health2").max}`;
   });
 
   function addMsgLog(msg){
@@ -243,15 +277,34 @@
   }
   function hit(obj1, obj2){
     let randomDamage = Math.floor(Math.random() * (obj1.maxDamage - obj1.min + 1)) + obj1.min;
-    obj2.currenthealth = obj2.currenthealth - (randomDamage * obj2.resistance);
-    addMsgLog(`${obj2.name} was damaged by ${obj1.name} from ${randomDamage * obj2.resistance} damages`)
+    obj2.currenthealth = obj2.currenthealth - Math.round(randomDamage * obj2.resistance);
+    addMsgLog(`${obj2.name} was damaged by ${obj1.name} from ${Math.round(randomDamage * obj2.resistance)} damages`)
     return obj2.currenthealth;
   }
   function heal(obj1){
-    let randomHeal = Math.floor(Math.random() * (obj1.maxHealth - obj1.min + 1)) + obj1.min;
-    obj1.currenthealth += (randomHeal * obj1.maxHealing);
-    addMsgLog(`${obj1.name} was heald by ${randomHeal} damages`);
-    return obj2.currenthealth;
+    let randomHeal = Math.floor(Math.random() * (obj1.maxHealing - obj1.min + 1)) + obj1.min;
+    if ((obj1.currenthealth + Math.round(randomHeal * obj1.boostHeal)) < obj1.maxHealth) {
+      obj1.currenthealth += Math.round(randomHeal * obj1.boostHeal);
+      addMsgLog(`${obj1.name} was heald by ${Math.round(randomHeal * obj1.boostHeal)}`);
+    } else if ((obj1.currenthealth + Math.round(randomHeal * obj1.boostHeal)) > obj1.maxHealth ){
+      obj1.currenthealth = obj1.maxHealth;
+      addMsgLog(`${obj1.name} was heald by ${Math.round(randomHeal * obj1.boostHeal)} but his life is full`);
+    }
+    return obj1.currenthealth;
   }
-  
+  function lifesteal(obj1, obj2){
+    let lifesteal = Math.round(obj2.currenthealth / 10 );
+    if ((obj1.currenthealth +lifesteal) < obj1.maxHealth ) {
+      obj1.currenthealth += lifesteal;
+      //obj2.currenthealth -= lifesteal;
+      addMsgLog(`${obj1.name} stole life from ${obj2.name} : ${lifesteal}`);
+    } else if ((obj1.currenthealth +lifesteal) > obj1.maxHealth) {
+      obj1.currenthealth = obj1.maxHealth;
+      //obj2.currenthealth -= lifesteal;
+      addMsgLog(`${obj1.name} stole life from ${obj2.name} : ${lifesteal} but life of ${obj1.name} is full`);
+    } 
+    
+    
+    
+  }
 })();
